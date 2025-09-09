4eddda5245c98745c2bf04ca4a84f50bde04bb66
Ella Pan <123600943+Ellaa02@users.noreply.github.com>
2025-09-09 08:57:42 -0400

    modify app.py and page1

--- Multipages/app.py
+++ Multipages/app.py
@@ -13,8 +13,6 @@ server = app.server
 app.layout = html.Div(
     [
         dbc.NavbarSimple(
-            #brand="Back to Home",
-            #brand_href="/",
             color="primary",          # navbar background color
             dark=True,                # light text on dark background
             expand="md",              # collapses on small screens, expands >= md
